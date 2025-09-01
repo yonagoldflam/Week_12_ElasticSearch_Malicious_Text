2d80a3c6378250078da92d5fb74aa172316f10cb
tzirkus <zalmen5779@gmail.com>
2025-09-01 09:57:15 +0300

    data loader

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -3,17 +3,29 @@ import csv
 
 class DataLoader:
     def __init__(self):
-        self.data = []
+        self.tweets_data = []
+        self.weapons = ''
 
-    def read_csv(self):
+
+    def read_tweets_csv(self):
         with open("../data/tweets_injected 3.csv", newline='', encoding='utf-8') as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
-                self.data.append(row)
+                self.tweets_data.append(row)
+
+    def read_weapons_txt(self):
+        with open("../data/weapon_list.txt", encoding='utf-8') as weapons:
+            self.weapons = weapons.read()
+
+
+
 
 
 
 d = DataLoader()
-d.read_csv()
-print(d.data[0].keys())
+d.read_tweets_csv()
+d.read_weapons_txt()
+print(d.tweets_data[0].keys())
+print(d.weapons)
+
 # dict_keys(['TweetID', 'CreateDate', 'Antisemitic', 'text'])
